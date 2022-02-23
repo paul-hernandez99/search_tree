490339e7816fe96c6689c24fde168668b6a017d7
fer <paul.hernandez@opendeusto.es>
2022-02-23 12:02:25 +0100

    script and output photos upload

---
+++ src/Search_tree.py
@@ -0,0 +1,190 @@
+graph = {
+'origen': ['A','B','C'],
+'A': ['D','E'],
+'B': ['F'],
+'C': ['G','H'],
+'D': [],
+'E': ['I','J'],
+'F': [],
+'G': [],
+'H': ['K','L'],
+'I': [],
+'J': ['M'],
+'K': ['N'],
+'L': [],
+'M': ['O'],
+'N': [],
+'O': []
+}
+
+class Node:
+    def __init__(self, parent=None, state=None, cost=0, depth=0, action=None):
+        self.parent = parent
+        self.state = state
+        self.cost = cost
+        self.depth = depth
+        self.action = action
+
+    def setParent(self, p):
+        self.parent = p
+
+    def setState(self, s):
+        self.state = s
+
+    def setCost(self, c):
+        self.cost = c
+
+    def setDepth(self, d):
+        self.depth = d
+
+    def setAction(self, a):
+        self.action = a
+
+
+def depth_search_ltr(first_state, goal):
+    print("Depth_search (from left to right):\n")
+    frontier = []
+    path = []
+    current_cost = 0
+    current_depth = 0
+    found = False
+
+    root = Node(state=first_state, action=graph[first_state])
+    frontier.append(root)
+
+    while not found and frontier:
+        current_node = frontier.pop()
+        path.append(current_node)
+        current_cost +=1
+        current_node.setCost(current_cost)
+        current_depth = current_node.depth
+
+        if(current_node.state == goal):
+            found = True
+            break
+        else:
+            getSuccessor(current_node, frontier, current_cost, current_depth)
+
+    printList(path)
+    print("cost: ",current_cost)
+    print("depth: ",current_depth)
+    print("-----\n\n")
+
+def depth_search_rtl(first_state, goal):
+    print("Depth_search (from right to left):\n")
+    frontier = []
+    path = []
+    current_cost = 0
+    current_depth = 0
+    found = False
+
+    root = Node(state=first_state, action=graph[first_state])
+    frontier.append(root)
+
+    while not found and frontier:
+        current_node = frontier.pop()
+        path.append(current_node)
+        current_cost +=1
+        current_node.setCost(current_cost)
+        current_depth = current_node.depth
+
+        if(current_node.state == goal):
+            found = True
+            break
+        else:
+            getSuccessor_reverse(current_node, frontier, current_cost, current_depth)
+
+    printList(path)
+    print("cost: ",current_cost)
+    print("depth: ",current_depth)
+    print("-----\n\n")
+
+def breadth_search_rtl(first_state, goal):
+    print("Breadth_search (from right to left):\n")
+    frontier = []
+    path = []
+    current_cost = 0
+    current_depth = 0
+    found = False
+
+    root = Node(state=first_state, action=graph[first_state])
+    frontier.append(root)
+
+    while not found and frontier:
+        current_node = frontier.pop(0)
+        path.append(current_node)
+        current_cost +=1
+        current_node.setCost(current_cost)
+        current_depth = current_node.depth
+
+        if(current_node.state == goal):
+            found = True
+            break
+        else:
+            getSuccessor(current_node, frontier, current_cost, current_depth)
+
+    printList(path)
+    print("cost: ",current_cost)
+    print("depth: ",current_depth)
+    print("-----\n\n")
+
+def breadth_search_ltr(first_state, goal):
+    print("Breadth_search (from left to right):\n")
+    frontier = []
+    path = []
+    current_cost = 0
+    current_depth = 0
+    found = False
+
+    root = Node(state=first_state, action=graph[first_state])
+    frontier.append(root)
+
+    while not found and frontier:
+        current_node = frontier.pop(0)
+        path.append(current_node)
+        current_cost +=1
+        current_node.setCost(current_cost)
+        current_depth = current_node.depth
+
+        if(current_node.state == goal):
+            found = True
+            break
+        else:
+            getSuccessor_reverse(current_node, frontier, current_cost, current_depth)
+
+    printList(path)
+    print("cost: ",current_cost)
+    print("depth: ",current_depth)
+    print("-----\n\n")
+
+
+def printList(list):
+    for element in list:
+        print(element.state)
+    print("----")
+
+def getSuccessor(node, frontier, current_cost, current_depth):
+    current_depth +=1
+    for n in node.action:
+        successor = Node(state=n)
+        successor.setParent(node.state)
+        successor.setCost(current_cost)
+        successor.setDepth(current_depth)
+        successor.setAction(graph[successor.state])
+        frontier.append(successor)
+
+def getSuccessor_reverse(node, frontier, current_cost, current_depth):
+    current_depth +=1
+    for n in node.action[::-1]:
+        successor = Node(state=n)
+        successor.setParent(node.state)
+        successor.setCost(current_cost)
+        successor.setDepth(current_depth)
+        successor.setAction(graph[successor.state])
+        frontier.append(successor)
+
+
+depth_search_ltr('origen','O')
+depth_search_rtl('origen','O')
+breadth_search_rtl('origen','O')
+breadth_search_ltr('origen','O')
